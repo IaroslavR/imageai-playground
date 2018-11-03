1e973c13f8d05a44470564519a62912e4ced47f8
iaroslav <iarruss@ya.ru>
2018-11-03 20:02:07 +0000

    [ref] simplified code

--- src/data/image_detection.py
+++ src/data/image_detection.py
@@ -1,19 +1,27 @@
+import logging
 import os
 import time
 from typing import List
 
 import attr
+import click
+import numpy as np
+import structlog
 from imageai.Detection import ObjectDetection
+from structlog_boilerplate import strict, colored, print_json
 
-PROJECT_ROOT = f"{os.path.dirname(os.path.abspath(__file__))}/../../"
+from src import PROJECT_ROOT
+
+logger = structlog.get_logger("image_detection")
+
+DETECTORS = ["resnet50", "yolo", "yolo-tiny"]
 
 
 @attr.s(auto_attribs=True)
-class ImageDetector(object):
-    type: str  # possible values are: resnet50 yolo yolo-tiny
+class ImageDetector:
+    name: str = attr.ib()
     models_path: str = os.path.join(PROJECT_ROOT, "data", "external")
     results_path: str = os.path.join(PROJECT_ROOT, "data", "processed")
-    detector: ObjectDetection = None
     probability: int = 30
     last_result: dict = None
     last_saved: str = None
@@ -27,20 +35,60 @@ class ImageDetector(object):
         "yolo": {"model": "yolo.h5", "type": "setModelTypeAsYOLOv3"},
         "yolo-tiny": {"model": "yolo-tiny.h5", "type": "setModelTypeAsTinyYOLOv3"},
     }
+    detector: ObjectDetection = attr.ib()
+    serialized_fields: List[str] = [
+        "name",
+        "last_result",
+        "last_saved",
+        "processing_time",
+    ]
 
-    def __attrs_post_init__(self):
-        self.detector = ObjectDetection()
-        getattr(self.detector, self.detector_model_map[self.type]["type"])()
-        self.detector.setModelPath(
-            os.path.join(
-                self.models_path, self.detector_model_map[self.type]["model"]
+    @detector.default
+    def init_detector(self):
+        logger.debug("Detector initialization started", type=self.name)
+        detector = ObjectDetection()
+        getattr(detector, self.detector_model_map[self.name]["type"])()
+        detector.setModelPath(
+            os.path.join(self.models_path, self.detector_model_map[self.name]["model"])
+        )
+        detector.loadModel()
+        logger.debug("Detector initialized")
+        return detector
+
+    @property
+    def serialized(self) -> dict:
+        """here we create serializable representation of processing results"""
+
+        def mutate(v):
+            if isinstance(v, np.integer):
+                return int(v)
+            elif isinstance(v, np.floating):
+                return float(v)
+            elif isinstance(v, np.ndarray):
+                return v.tolist()
+            return v
+
+        def convert(data: dict) -> dict:
+            converted = {}
+            for k, v in data.items():
+                if isinstance(v, dict):
+                    v = convert(v)
+                elif isinstance(v, list):
+                    v = [convert(i) if isinstance(i, dict) else mutate(i) for i in v]
+                else:
+                    v = mutate(v)
+                converted[k] = v
+            return converted
+
+        return convert(
+            attr.asdict(
+                self, filter=lambda attr, _: attr.name in self.serialized_fields
             )
         )
-        self.detector.loadModel()
 
-    def run(self, img_fname):
+    def run(self, img_fname: str) -> dict:
         processed_fname = os.path.join(
-            self.results_path, f"{self.type}_{os.path.basename(img_fname)}"
+            self.results_path, f"{self.name}_{os.path.basename(img_fname)}"
         )
         ts = time.time()
         self.last_result = self.detector.detectObjectsFromImage(
@@ -50,16 +98,34 @@ class ImageDetector(object):
         )
         self.processing_time = time.time() - ts
         self.last_saved = processed_fname
-        return self.last_result
+        r = self.serialized
+        return r
+
+
+@click.command()
+@click.argument("detector_type", type=click.Choice(DETECTORS))
+@click.argument(
+    "image",
+    default=f"{PROJECT_ROOT}/data/interim/road_camera_640x480_0-17.jpeg",
+    type=click.Path(exists=True),
+)
+@click.option("-v", "--verbose", count=True)
+def cli(detector_type, image, verbose):
+    level = {0: logging.ERROR, 1: logging.INFO, 2: logging.DEBUG}
+    verbose = min(verbose, 2)
+    if verbose:
+        colored(level[verbose])
+    else:
+        strict(level[verbose])
+    try:
+        detector = ImageDetector(detector_type)
+        logger.debug("Object detection started", path=image)
+        print_json(detector.run(image))
+    except Exception:
+        logger.exception("Unhandled processing error")
+    else:
+        logger.debug("Object detection complete")
 
 
 if __name__ == "__main__":
-    img_fname = f"{PROJECT_ROOT}/data/interim/road_camera_640x480_0-17.jpeg"
-    detectors = [
-        ImageDetector("resnet50"),
-        ImageDetector("yolo"),
-        ImageDetector("yolo-tiny"),
-    ]
-    for detector in detectors:
-        detector.run(img_fname)
-        print(detector.type, detector.processing_time)
+    cli()
